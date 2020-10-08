929463634e82aef46c5e6af9de7df07e872d7a5d
Matheus Acosta <matheusthebr@gmail.com>
2020-10-07 22:59:55 -0300

    Load recipe as nibble, and send warning for unauthorized requests.

--- server.rb
+++ server.rb
@@ -1,28 +1,26 @@
 require 'open3'
 require 'sinatra'
 require 'sinatra/namespace'
+require 'json'
 
 class Server < Sinatra::Application
   namespace '/api/v1' do
     before do
       content_type 'application/json'
+      return_unauthorized if invalid_request?
 
-      halt(401) if invalid_request?
+      request.body.rewind
+      params = JSON.parse(request.body.read, symbolize_names: true)
+
+      @nibble = params[:environment].to_i(2).to_s(16)
     end
 
     post '/deploy' do
-      nibble = params[:environment].to_i(2).to_s(16)
-      recipe = "./recipes/#{ENV["recipe_#{nibble}"]}"
+      recipe = "#{__dir__}/recipes/#{ENV["RECIPE_#{@nibble}"]}"
 
       stderr = Open3.capture3(recipe)[1]
 
-      if stderr.empty?
-        `wall DEPLOY SCHEDULED!`
-        status 201
-      else
-        `wall DEPLOY ERROR: #{stderr}`
-        status 500
-      end
+      stderr.empty? ? return_success : return_failure(stderr)
     end
   end
 
@@ -32,5 +30,20 @@ class Server < Sinatra::Application
     return true if request.env['HTTP_USER_AGENT'] != 'ESP8266HTTPClient'
 
     request.env['HTTP_API_KEY'] != ENV['API_KEY']
+  end
+
+  def return_unauthorized
+    `wall WARNING, INTRUDER REQUEST FROM #{request.ip} 0o`
+    halt(401)
+  end
+
+  def return_success
+    `wall DEPLOY SCHEDULED!`
+    status 201
+  end
+
+  def return_failure(error)
+    `wall DEPLOY ERROR: #{error}`
+    status 500
   end
 end
